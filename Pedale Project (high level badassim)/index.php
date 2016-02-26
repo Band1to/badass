6efdf43f86d4e33561031688c2e7a94a71b0c8cb
Evgeniy Tkachenko <et.coder@gmail.com>
2016-02-27 00:22:53 +0300

    :ear_of_rice: Enhancement LaTortue
    Added baddasim centration.
    Added baddasim animation.
    Improved baddasim state of PHP.
    
    Related of #6 #7 #8 #9 #10 #21 #23 #24 #26 #28

--- Pedale Project (high level badassim)/index.php
+++ Pedale Project (high level badassim)/index.php
@@ -1,21 +1,37 @@
+<?php
+$art = file('asciiart.txt', FILE_IGNORE_NEW_LINES);
+$badAssArt = '';
+$spanTag = '<span class="magnifico" style="color:#%s;">%s</span>';
+foreach ($art as $line) {
+    foreach (str_split($line) as $char) {
+        $badAssArt .= sprintf($spanTag, substr(md5(rand()), 0, 6), $char);
+    }
+    $badAssArt .= PHP_EOL;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
+    <style>
+        @keyframes open_your_eyes {
+            to {
+                color: #2B3336;
+            }
+        }
+        .magnifico {
+            animation-name: open_your_eyes;
+            animation-duration: 1s;
+            animation-iteration-count: infinite;
+        }
+    </style>
 </head>
 <body style="background-color:#2B3336;">
-<pre>
-<?php
-//Pedale project
-$couleurpedale = array("#FF85BC","#CCFF00","#FFCE36","#01FFCC","#FFEB88","#FF8584"); //6
-$art = file('asciiart.txt', FILE_IGNORE_NEW_LINES );
-foreach($art as $line){
-        for($i = 0; $i < strlen($line); $i++){
-                $number = $i % count($couleurpedale);
-                echo '<span style="color:'.$couleurpedale[$number].';">'.$line[$i].'</span>';
-        }
-        echo PHP_EOL;
-}
-?>
+<pre style="position: absolute;  top: 50%;  left: 50%;  transform: translate(-50%, -50%);">
+<?= $badAssArt ?>
+<main>
+<h3>Nothing really matters - nothing really matters to me<h3>
+<marquee behavior="scroll" direction="left" style="color:#ffcc00">Anyway the wind blows...</marquee>
+</main>
 </pre>
 </body>
 </html>
